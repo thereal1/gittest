9b70c62edd10b9611d4bc74506f804a56d3372c6
Zac Workman <zac.workman45@gmail.com>
2017-04-18 13:47:49 +0100

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Zac Workman on 18/04/2017.
+//  Copyright © 2017 Zac Workman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
